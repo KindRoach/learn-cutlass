a2314f1401946f346e75f75b00a52d0422996e44
KindRoach <kindroach@hotmail.com>
2025-10-08 22:38:07 +0800

    Enhance tuple example with additional tuples and detailed output

--- src/cute/001-basic/tuple.cu
+++ src/cute/001-basic/tuple.cu
@@ -7,23 +7,38 @@ using namespace cute;
 int main()
 {
     // create tuples
-    auto t1 = make_tuple(int{2}, Int<3>{});
-    auto t2 = make_tuple(uint16_t{42}, make_tuple(Int<1>{}, int32_t{3}), Int<17>{});
+    auto t0 = 1;
+    auto t1 = make_tuple(int{1});
+    auto t2 = make_tuple(int{2}, Int<3>{});
+    auto t3 = make_tuple(int{4}, make_tuple(Int<5>{}, int32_t{6}), Int<7>{});
 
-    // rank
-    std::cout << "t1 rank: " << rank(t1) << "\n";
-    std::cout << "t2 rank: " << rank(t2) << "\n";
+    // get item
+    auto e1 = get<0>(t2); // dynamic 2
+    auto e2 = get<1>(t2); // static 3
+    std::cout << "t2 elements: " << e1 << ", " << e2.value << "\n";
+    std::cout << "\n";
 
-    // depth
-    std::cout << "t1 depth: " << depth(t1) << "\n";
-    std::cout << "t2 depth: " << depth(t2) << "\n";
+    std::cout << "tuple: " << t0 << "\n";
+    std::cout << "  rank : " << rank(t0) << "\n";
+    std::cout << "  depth : " << depth(t0) << "\n";
+    std::cout << "  size : " << size(t0) << "\n";
+    std::cout << "\n";
 
-    // get
-    auto e1 = get<0>(t1); // dynamic 2
-    auto e2 = get<1>(t1); // static 3
-    std::cout << "t1 elements: " << e1 << ", " << e2.value << "\n";
+    std::cout << "tuple: " << t1 << "\n";
+    std::cout << "  rank : " << rank(t1) << "\n";
+    std::cout << "  depth : " << depth(t1) << "\n";
+    std::cout << "  size : " << size(t1) << "\n";
+    std::cout << "\n";
 
-    // size
-    std::cout << "t1 size: " << size(t1) << "\n";
-    std::cout << "t2 size: " << size(t2) << "\n";
+    std::cout << "tuple: " << t2 << "\n";
+    std::cout << "  rank : " << rank(t2) << "\n";
+    std::cout << "  depth : " << depth(t2) << "\n";
+    std::cout << "  size : " << size(t2) << "\n";
+    std::cout << "\n";
+
+    std::cout << "tuple: " << t3 << "\n";
+    std::cout << "  rank : " << rank(t3) << "\n";
+    std::cout << "  depth : " << depth(t3) << "\n";
+    std::cout << "  size : " << size(t3) << "\n";
+    std::cout << "\n";
 }

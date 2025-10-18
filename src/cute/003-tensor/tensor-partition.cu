4abf756956b1bdacefc5cf78f94c884551756ae1
KindRoach <kindroach@hotmail.com>
2025-10-18 13:42:05 +0800

    Add tensor-partition.cu with inner and outer partition example.

---
+++ src/cute/003-tensor/tensor-partition.cu
@@ -0,0 +1,74 @@
+#include <cute/tensor.hpp>
+
+
+template <class Tensor, class Tiler>
+void inner_partition_with_print(Tensor A, Tiler tiler)
+{
+    using namespace cute;
+
+    std::cout << "=============== inner partition ===============\n";
+
+    auto rest_shape = shape<1>(zipped_divide(layout(A), tiler));
+    std::cout << "tiler = " << tiler << "\n";
+    std::cout << "tile_layout = " << rest_shape << "\n";
+    std::cout << "\n";
+
+    std::cout << "A = ";
+    print_tensor(A);
+    std::cout << "\n";
+
+    for (int i = 0; i < size(rest_shape); ++i)
+    {
+        auto coord = idx2crd(i, rest_shape);
+
+        // identical to local_tile(A , tiler, coord)
+        auto inner_a = inner_partition(A, tiler, coord);
+
+        std::cout << "inner_partition(A, tiler, " << coord << ") = ";
+        print_tensor(inner_a);
+        std::cout << "\n";
+    }
+}
+
+template <class Tensor, class Tiler>
+void outer_partition_with_print(Tensor A, Tiler tiler)
+{
+    using namespace cute;
+
+    std::cout << "=============== outer partition ===============\n";
+
+    std::cout << "tiler = " << tiler << "\n";
+    std::cout << "\n";
+
+    std::cout << "A = ";
+    print_tensor(A);
+    std::cout << "\n";
+
+    for (int i = 0; i < size(tiler); ++i)
+    {
+        auto coord = idx2crd(i, tiler);
+
+        // identical to local_tile(A , tiler, coord)
+        auto outer_a = outer_partition(A, tiler, coord);
+
+        std::cout << "outer_partition(A, tiler, " << coord << ") = ";
+        print_tensor(outer_a);
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    using namespace cute;
+
+    auto A = make_tensor<int>(Shape<_4, _8>{});
+    for (int i = 0; i < size(A); ++i)
+    {
+        A[i] = i;
+    }
+
+    auto tiler = Shape<_2, _4>{};
+
+    inner_partition_with_print(A, tiler);
+    outer_partition_with_print(A, tiler);
+}
